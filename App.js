630aeacf2838735ecb680e3ca712ff5e8e236a9d
flyingsnoopy <lijiaqi1835@outlook.com>
2024-06-11 01:04:17 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from "twrnc";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={tw.style`flex flex-col gap-2 w-11/12`}>
+        <View style={tw.style`flex flex-row justify-between w-full`}>
+          <Text style={tw.style`text-gray-400 w-1/6`}>12:00</Text>
+          <View style={tw.style`flex flex-col w-2/3`}>
+            <Text></Text>
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"flex-start",
+    marginTop:'0.75rem',
   },
 });
